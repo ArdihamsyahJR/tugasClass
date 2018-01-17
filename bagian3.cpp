f1f7114981ee04fee464a5f106ae13d3779500df
ArdihamsyahJR <32383340+ArdihamsyahJR@users.noreply.github.com>
2018-01-17 14:55:08 +0700

    bagian3_mengisi_variable_merek
    bagian3_mengisi_variable_merek

---
+++ bagian3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class sendal {
+	public:
+	void perbedaan(string jenis);
+	int harga;
+	string merek;
+};
+
+int main(){
+		sendal nipon, nike, adidas;
+		
+	nipon.merek="nipon sendal masyarakat baik";
+	nike.merek="nike sandal masyarakat keren";
+	adidas.merek="adidas sandal masyarakat terbaik dan keren ";
